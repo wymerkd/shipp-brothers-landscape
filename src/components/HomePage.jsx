70ecbde4ebebae08efebec1142fe0fecd5b985ec
wymerkd <wymerkd@gmail.com>
2020-03-05 10:09:12 -0800

    create components for corresponding pages, add content to navbar component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import NavBar from './NavBar'
 
-const Home = () => {
+const HomePage = () => {
   return(
     <div>
+      <NavBar />
     </div>
   );
 }
 
-export default Home;
+export default HomePage;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Navbar, NavDropdown, Nav } from 'react-bootstrap'
+
+const NavBar = () => {
+  return(
+    <div>
+      <Navbar id="newNav" collapseOnSelect expand="lg">
+    <Navbar.Brand href="#/"><img className="img-fluid" src="/images/treeLogo.png" alt=""></img><h1 id="brandLogo">SHIPP BROTHERS LANDSCAPE</h1></Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+    </Nav>
+    <Nav>
+      <Nav.Link id="newNav" href="#/">Home</Nav.Link>
+      <Nav.Link href="#about">About</Nav.Link>
+      <Nav.Link href="#gallery">Gallery</Nav.Link>
+      <NavDropdown title="Services" id="collapsible-nav-dropdown">
+        <NavDropdown.Item id="huh" href="#action/3.1">Custom Landscape Design</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Lawn Installation</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Water Features & Waterfalls</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Retaining Walls & Steps</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Lighting</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Bark Blowing</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#gallery">Lawn Care Guide</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link href="#/">Nursery Stock</Nav.Link>
+      <Nav.Link href="#contact">Contact</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+    </div>
+  );
+}
+
+export default NavBar;
