339e91e03e196db1f08a5f46432016ca257ead2f
wymerkd <wymerkd@gmail.com>
2020-03-07 15:31:25 -0800

    add images and videos for galleries.

--- src/components/GalleryPhotos.jsx
+++ src/components/GalleryPhotos.jsx
@@ -2,73 +2,137 @@ import React from 'react';
 
 export const photos = [
   {
-    src: "/images/galleryImages/shipp8.png",
+    src: "/images/galleryImages/gallery1.png",
     width: 4,
     height: 3,
-    title: "yo"
   },
   {
-    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    src: "/images/galleryImages/gallery2.png",
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    src: "/images/galleryImages/gallery3.png",
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    src: "/images/galleryImages/gallery4.png",
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    src: "/images/galleryImages/gallery5.png",
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    src: "/images/galleryImages/gallery6.png",
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    src: "/images/galleryImages/gallery7.png",
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    src: "/images/galleryImages/gallery8.png",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "/images/galleryImages/gallery9.png",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "/images/galleryImages/gallery10.png",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "/images/galleryImages/gallery11.png",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "/images/galleryImages/gallery15.png",
     width: 3,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    src: "/images/galleryImages/gallery12.png",
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    src: "/images/galleryImages/gallery13.png",
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    src: "/images/galleryImages/gallery14.png",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "/images/galleryImages/gallery16.png",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "/images/galleryImages/gallery18.png",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "/images/galleryImages/gallery19.png",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "/images/galleryImages/gallery17.png",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "/images/galleryImages/gallery20.png",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "/images/galleryImages/gallery21.png",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "/images/galleryImages/gallery22.png",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "/images/galleryImages/gallery23.png",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "/images/galleryImages/gallery24.png",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "/images/galleryImages/gallery25.png",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "/images/galleryImages/gallery26.png",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "/images/galleryImages/gallery27.png",
     width: 4,
     height: 3
   }
