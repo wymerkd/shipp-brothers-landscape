aa642413285783b05345bd2cf63b652c442c492f
wymerkd <wymerkd@gmail.com>
2020-03-09 12:09:56 -0700

    add styling to header fonts and adjust padding

--- src/components/TeamHeader.jsx
+++ src/components/TeamHeader.jsx
@@ -6,7 +6,7 @@ const TeamHeader = () => {
   return(
     <div className="allHeader" id="aboutHeader">
       <div className="container-fluid padding">
-        <div className="row header text-center">
+        <div className="row teamHeader text-center">
           <div className="col-12">
             <h1 className="display-4">Meet The Team</h1>
           </div>
