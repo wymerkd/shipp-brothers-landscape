99c84aafaa9b337fdb7f8c67569e9027ab93567e
wymerkd <wymerkd@gmail.com>
2020-03-10 09:57:13 -0700

    change map size for mobile

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -4,22 +4,20 @@ import '../assets/stylesheets/AboutUs.css';
 const AboutUs = () => {
   return(
     <div>
-      <div id="aboutUs">
-        <div className="container-fluid">
-          <div className="about row">
-            <div className="col-lg-6">
-              <h1>Who We Are</h1>
-              <p>Shipp Brothers is proud to display the largest nursery in all of Clark County. We have over 50 acres of field grown stock, and 10 green-houses full of beautiful, professionally maintained container stock.</p>
-              <br></br>
-              <p>Shipp Brothers employs 10-15 workers, depending on seasonal weather. All employees receive the same professional training to ensure that the quality of our service is consistently superior.</p>
-              <br></br>
-              <p>Shipp Brothers Landscape is a member of the Washington Association of Landscape Professionals (WALP).</p>
-              <br></br>
-              <br></br>
-            </div>
-            <div className="col-lg-6">
-              <img src="/images/galleryImages/shipp6.png" className="imageShadow img-fluid mb-5 rounded" alt="" />
-            </div>
+      <div className="container-fluid">
+        <div className="about row">
+          <div className="col-lg-6">
+            <h1>Who We Are</h1>
+            <p>Shipp Brothers is proud to display the largest nursery in all of Clark County. We have over 50 acres of field grown stock, and 10 green-houses full of beautiful, professionally maintained container stock.</p>
+            <br></br>
+            <p>Shipp Brothers employs 10-15 workers, depending on seasonal weather. All employees receive the same professional training to ensure that the quality of our service is consistently superior.</p>
+            <br></br>
+            <p>Shipp Brothers Landscape is a member of the Washington Association of Landscape Professionals (WALP).</p>
+            <br></br>
+            <br></br>
+          </div>
+          <div className="col-lg-6">
+            <img src="/images/galleryImages/shipp6.png" className="imageShadow img-fluid mb-5 rounded" alt="" />
           </div>
         </div>
       </div>
