379dd747c3328f2a8f3c08fee9d8543f70fcb959
wymerkd <wymerkd@gmail.com>
2020-03-10 23:46:05 -0700

    for heroku

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -8,6 +8,7 @@ const AboutUs = () => {
         <div className="about row">
           <div className="col-lg-6">
             <h1>Who We Are</h1>
+            <br></br>
             <p>Shipp Brothers is proud to display the largest nursery in all of Clark County. We have over 50 acres of field grown stock, and 10 green-houses full of beautiful, professionally maintained container stock.</p>
             <br></br>
             <p>Shipp Brothers employs 10-15 workers, depending on seasonal weather. All employees receive the same professional training to ensure that the quality of our service is consistently superior.</p>
